1106c0d8050900962a146273caeb25f356331567
Dominika Vašíčková <vasickova.dominika19@gmail.com>
2022-03-21 10:38:38 +0100

    Ukol

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Shoes from './components/Shoes';
+import Dresses from './components/Dresses';
+import Accessories from './components/Accessories'
+
 const App = () => (
-  <>
-    <p>Tady asi něco bude</p>
-  </>
+  <div class="container">
+    <Header title='Móda'/>
+    <Dresses />
+    <Shoes />
+    <Accessories />
+    <Footer year='2022' author='Czechitas'/>
+  </div> 
 );
 
 render(<App />, document.querySelector('#app'));
